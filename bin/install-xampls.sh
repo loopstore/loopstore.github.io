053dfbab434ae7136ad0526350f1850aec2c211c
cloudWheels <44451128+cloudwheels@users.noreply.github.com>
2018-12-06 19:25:08 +0000

    Create install-xampls.sh

---
+++ bin/install-xampls.sh
@@ -0,0 +1,23 @@
+#accept repo / project name as first parameter
+PROJECT_NAME_DEFAULT="my-new-project"
+$PROJECT_NAME_DEFAULT=$1
+
+#create project directory
+mkdir ~/$PROJECT_NAME_DEFAULT && cd $_
+ECHO "created project in" $_
+
+
+#get license file
+
+
+
+#create a 
+
+
+#curl
+
+#add exec permission
+chmod +x ~/usrbin/claat
+
+#export path
+export PATH=$PATH:~/usrbin/
